8d5c2caaef62ce7d4c4391e662c64178dd00cbbf
rchinmaye19-alt <rchinmaye19@gmail.com>
2026-02-11 12:35:10 +0530

    have done

---
+++ basic_patterns.java
@@ -0,0 +1,25 @@
+class RectanglePattern {
+    public static void main (String [] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+class RightTringle {
+
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            
+            }
+            System.out.println();
+
+        }
+    }
+}
