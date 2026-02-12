743ca61da0f93f2808cce878f50b0a3743c5cd3c
rchinmaye19-alt <rchinmaye19@gmail.com>
2026-02-12 10:05:23 +0530

    arrays done

--- demo.java
+++ demo.java
@@ -36,3 +36,41 @@ class JaggedArrayExample {
         }
     }
 }
+class demo2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+
+
+}
+   j
+class ArrayMax{
+    public static void main(String[] args) {
+        int[] arr = {10,30,60,80 };
+        int max = 30;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("max:"+max);
+    }
+}
+ class ArrayMax{
+    public static void main(String[] args) {
+        int[] arr = {10,30,60,80};
+        int max=0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]>max) {
+                max=arr[i];
+            }
+        }
+            System.out.println("max:"+max);
+
+    }
+}
